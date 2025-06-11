fddbb2e106768cf9e18c05a4747016344ef7dc9f
SGGregory76 <sggregory76@gmail.com>
2025-06-10 22:34:12 -0700

    Update config.js

--- config.js
+++ config.js
@@ -9,18 +9,18 @@ window.CONFIG = {
       mainImage: "assets/images/03311262-18a0-4e24-b4bb-ed73b2d472b9.png",
       frames: [
         "assets/frames/placeholder/1.png",
-        "assets/frames/placeholder/2.png",
-        "assets/frames/placeholder/3.png"
+        "assets/frames/placeholder/03311262-18a0-4e24-b4bb-ed73b2d472b9.png",
+        "assets/frames/placeholder/1.png"
       ]
     },
     {
       name:      "boing",
       label:     "Boing SFX",
       audio:     "assets/audio/ticking-clock-108342.mp3",
-      mainImage: "assets/images/boing-main.png",
+      mainImage: "assets/frames/placeholder/1.png",
       frames: [
         "assets/frames/placeholder/1.png",
-        "assets/frames/boing/2.png",
+        "assets/frames/placeholder/03311262-18a0-4e24-b4bb-ed73b2d472b9.png",
         "assets/frames/boing/3.png"
       ]
     }
