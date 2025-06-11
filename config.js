043c9ef54ed8bc7372c1b18b1c5b77fc07c15c11
SGGregory76 <sggregory76@gmail.com>
2025-06-10 20:54:25 -0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ window.CONFIG = {
     {
       name:   "laugh",
       label:  "Laugh SFX",
-      audio:  "https://upload.wikimedia.org/wikipedia/commons/c/c8/Beep_sound_effect_-_1s.mp3",
+      audio:  "assets/audio/ticking-clock-108342.mp3",
       frames: [
         "https://SGGregory76.github.io/sound-comic-widget/assets/images/03311262-18a0-4e24-b4bb-ed73b2d472b9.png",
         "https://SGGregory76.github.io/sound-comic-widget/assets/frames/placeholder/2.png",
