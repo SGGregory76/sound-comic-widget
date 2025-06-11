5e8b08c8c386cd3f0b84de75a8ce74ecb3cd27fa
SGGregory76 <sggregory76@gmail.com>
2025-06-10 20:47:22 -0700

    Update and rename config.json to config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+// https://yourusername.github.io/sound-comic-widget/config.js
+window.CONFIG = {
+  items: [
+    {
+      name:   "laugh",
+      label:  "Laugh SFX",
+      audio:  "https://upload.wikimedia.org/wikipedia/commons/c/c8/Beep_sound_effect_-_1s.mp3",
+      frames: [
+        "https://SGGregory76.github.io/sound-comic-widget/assets/frames/placeholder/1.png",
+        "https://SGGregory76.github.io/sound-comic-widget/assets/frames/placeholder/2.png",
+        "https://SGGregory76.github.io/sound-comic-widget/assets/frames/placeholder/3.png"
+      ]
+    }
+    // …add more here…
+  ]
+};
