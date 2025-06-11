85bd76428a744c3be480539c5c8e78bc359de427
SGGregory76 <sggregory76@gmail.com>
2025-06-10 21:44:30 -0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ window.CONFIG = {
       name:      "placeholder",
       label:     "Demo SFX",
       audio:     "https://sggregory76.github.io/sound-comic-widget/assets/audio/ticking-clock-108342.mp3",
-      mainImage: "https://sggregory76.github.io/sound-comic-widget/assets/images/placeholder-main.png",
+      mainImage: "https://sggregory76.github.io/sound-comic-widget/assets/images/",
       frames: [
         "https://sggregory76.github.io/sound-comic-widget/assets/frames/placeholder/1.png",
         "https://sggregory76.github.io/sound-comic-widget/assets/frames/placeholder/2.png",
