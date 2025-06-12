50a8a1293a4d13b3819f4024bce653c08d8d8077
SGGregory76 <sggregory76@gmail.com>
2025-06-11 18:08:26 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 export const config = {
   sounds: [
     {
-      name: 'Tick',
-      audioPath: 'https://SGGregory76.github.io/sound-comic-widget/assets/audio/assets/audio/alien-message-received-108372.mp3',
-      framesPath: 'https://SGGregory76.github.io/sound-comic-widget/assets/frames/placeholder/03311262-18a0-4e24-b4bb-ed73b2d472b9.png',
-      frameCount: 3
-    },
-    {
-      name: 'Pop',
-      audioPath: 'https://SGGregory76.github.io/sound-comic-widget/assets/audio/Boing.m4a',
-      framesPath: 'https://SGGregory76.github.io/sound-comic-widget/assets/frames/placeholder/260a7a9e-b855-428d-b862-84f6189fdd7d.png',
-      frameCount: 3
+      name: "placeholder",
+      label: "Demo SFX",
+      audio: "assets/audio/ticking-clock-108342.mp3",
+      mainImg: "assets/frames/placeholder/1.png",
+      panels: [
+        "assets/frames/placeholder/1.png",
+        "assets/frames/placeholder/2.png",
+        "assets/frames/placeholder/3.png"
+      ]
     }
-  ],
-  frameIntervalMs: 300,
-  placeholderFrame: 'https://SGGregory76.github.io/sound-comic-widget/assets/frames/placeholder/1.png',
-  mainImage: 'https://SGGregory76.github.io/sound-comic-widget/assets/images/03311262-18a0-4e24-b4bb-ed73b2d472b9.png'
+    // Add more items here
+  ]
 };
