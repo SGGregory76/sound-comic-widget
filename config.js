7ffd940f5100b600c8caaf451345cca0e5ade16c
SGGregory76 <sggregory76@gmail.com>
2025-06-11 17:46:06 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const config = {
     {
       name: 'Tick',
       audioPath: 'https://SGGregory76.github.io/sound-comic-widget/assets/audio/assets/audio/alien-message-received-108372.mp3',
-      framesPath: 'https://SGGregory76.github.io/sound-comic-widget/assets/frames/tick/',
+      framesPath: 'https://SGGregory76.github.io/sound-comic-widget/assets/frames/placeholder/03311262-18a0-4e24-b4bb-ed73b2d472b9.png',
       frameCount: 3
     },
     {
