214eb023b81b897e7781e491650555cbc36dc487
SGGregory76 <sggregory76@gmail.com>
2025-06-11 17:43:40 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,19 @@
 export const config = {
   sounds: [
     {
-      name: 'Alien',
-      audioPath: 'assets/audio/alien-message-received-108372.mp3',
-      framesPath: 'assets/frames/alien/',
+      name: 'Tick',
+      audioPath: 'https://SGGregory76.github.io/sound-comic-widget/assets/audio/tick.mp3',
+      framesPath: 'https://SGGregory76.github.io/sound-comic-widget/assets/frames/tick/',
       frameCount: 3
     },
     {
-      name: 'Boing',
-      audioPath: 'assets/audio/Boing.m4a',
-      framesPath: 'assets/frames/boing/',
+      name: 'Pop',
+      audioPath: 'https://SGGregory76.github.io/sound-comic-widget/assets/audio/pop.mp3',
+      framesPath: 'https://SGGregory76.github.io/sound-comic-widget/assets/frames/pop/',
       frameCount: 3
     }
   ],
-  frameIntervalMs: 300
+  frameIntervalMs: 300,
+  placeholderFrame: 'https://SGGregory76.github.io/sound-comic-widget/assets/frames/placeholder/1.png',
+  mainImage: 'https://SGGregory76.github.io/sound-comic-widget/assets/images/main.png'
 };
