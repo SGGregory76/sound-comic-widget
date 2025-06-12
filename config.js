960baa82934abb0a2dc6977a36d4bb9075c6b361
SGGregory76 <sggregory76@gmail.com>
2025-06-11 19:05:06 -0700

    Update config.js

--- config.js
+++ config.js
@@ -4,17 +4,17 @@ export const config = {
       name: "Alien",
       label: "Alien SFX",
       audio: "assets/audio/alien-message-received-108372.mp3",
-      mainImg: "assets/frames/placeholder/1.png",
+      mainImg: "assets/frames/placeholder/cb4b4bc5-eb5c-47bb-88dd-723cdffdf4b7.png",
       panels: [
-        "assets/frames/placeholder/1.png",
-        "assets/frames/placeholder/2.png",
-        "assets/frames/placeholder/3.png"
+        "assets/frames/placeholder/03311262-18a0-4e24-b4bb-ed73b2d472b9.png",
+        "assets/frames/placeholder/03311262-18a0-4e24-b4bb-ed73b2d472b9.png",
+        "assets/frames/placeholder/036e2a5d-11a9-445c-8480-6d57fad4ac32.png"
       ]
     },
     {
       name: "pop",
       label: "Pop SFX",
-      audio: "assets/audio/pop.mp3",
+      audio: "assets/audio/trashcan-108244.mp3",
       mainImg: "assets/frames/pop/1.png",
       panels: [
         "assets/frames/pop/1.png",
