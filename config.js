be88cfcec4f98c70da22f6fbcc87b2ecc32c7a39
SGGregory76 <sggregory76@gmail.com>
2025-06-16 20:21:11 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,28 +1,16 @@
 window.CONFIG = {
   scenes: [
     {
-      title: "Chicken Fight",
-      mainImage: "assets/images/chicken.png",
-      sounds: [
-        { label: "Squawk!", file: "assets/audio/squawk.mp3" },
-        { label: "Crash!", file: "assets/audio/crash.mp3" }
-      ],
-      frames: [
-        "assets/frames/chicken/1.png",
-        "assets/frames/chicken/2.png"
-      ]
+      image: "assets/images/chicken.png",
+      sound: "assets/audio/squawk.mp3"
     },
     {
-      title: "Pizza Panic",
-      mainImage: "assets/images/pizza.png",
-      sounds: [
-        { label: "Yum!", file: "assets/audio/yum.mp3" },
-        { label: "Hot Sauce!", file: "assets/audio/sizzle.mp3" }
-      ],
-      frames: [
-        "assets/frames/pizza/1.png",
-        "assets/frames/pizza/2.png"
-      ]
+      image: "assets/images/pizza.png",
+      sound: "assets/audio/yum.mp3"
+    },
+    {
+      image: "assets/images/burrito.png",
+      sound: "assets/audio/boom.mp3"
     }
   ]
 };
