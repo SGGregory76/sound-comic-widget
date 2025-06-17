6207c50f0d49aa1af7220999379f50b2d50042fb
SGGregory76 <sggregory76@gmail.com>
2025-06-16 20:37:09 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,12 @@
 window.CONFIG = {
   scenes: [
     {
-      image: "https://raw.githubusercontent.com/SGGregory76/SGGregoryStudio/main/assets/images/de05170e-c7c2-4bad-9476-46d3a0f45698.png",
-      sound: "https://raw.githubusercontent.com/SGGregory76/SGGregoryStudio/main/assets/audio/arnold-schwarzenegger---i-want-to-ask-you-a-bunch-of-questions-made-with-Voicemod.mp3"
+      image: "https://raw.githubusercontent.com/SGGregory76/sound-comic-widget/main/assets/images/chicken.png",
+      sound: "https://raw.githubusercontent.com/SGGregory76/sound-comic-widget/main/assets/audio/squawk.mp3"
     },
     {
-      image: "https://raw.githubusercontent.com/SGGregory76/SGGregoryStudio/main/assets/images/ecbbc098-2535-48f1-acfa-ec26fad2c7d2.png",
-      sound: "https://raw.githubusercontent.com/SGGregory76/SGGregoryStudio/main/assets/audio/epic-logo-6906.mp3"
-    },
-    {
-      image: "https://raw.githubusercontent.com/SGGregory76/SGGregoryStudio/main/assets/images/36563c55-2a48-43db-8851-4289a46e3763.png",
-      sound: "https://raw.githubusercontent.com/SGGregory76/SGGregoryStudio/main/assets/audio/arnold-schwarzenegger---i-want-to-ask-you-a-bunch-of-questions-made-with-Voicemod.mp3"
+      image: "https://raw.githubusercontent.com/SGGregory76/sound-comic-widget/main/assets/images/pizza.png",
+      sound: "https://raw.githubusercontent.com/SGGregory76/sound-comic-widget/main/assets/audio/yum.mp3"
     }
   ]
 };
