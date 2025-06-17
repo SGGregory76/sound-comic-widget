99d3d985a16a181f94aaa1498d01d28bebf5afd3
SGGregory76 <sggregory76@gmail.com>
2025-06-16 20:48:28 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 window.CONFIG = {
   scenes: [
     {
-      image: "https://raw.githubusercontent.com/SGGregory76/sound-comic-widget/main/assets/images/chicken.png",
-      sound: "https://raw.githubusercontent.com/SGGregory76/sound-comic-widget/main/assets/audio/squawk.mp3"
+      image: "https://raw.githubusercontent.com/SGGregory76/sound-comic-widget/main/assets/frames/placeholder/ecbbc098-2535-48f1-acfa-ec26fad2c7d2.png",
+      sound: "https://raw.githubusercontent.com/SGGregory76/sound-comic-widget/main/assets/audio/arnold-schwarzenegger---i-want-to-ask-you-a-bunch-of-questions-made-with-Voicemod.mp3"
     },
     {
-      image: "https://raw.githubusercontent.com/SGGregory76/sound-comic-widget/main/assets/images/pizza.png",
-      sound: "https://raw.githubusercontent.com/SGGregory76/sound-comic-widget/main/assets/audio/yum.mp3"
+      image: "https://raw.githubusercontent.com/SGGregory76/sound-comic-widget/main/assets/images/2a82ade3-569d-4a55-af3a-7e61d8306add.png",
+      sound: "https://raw.githubusercontent.com/SGGregory76/sound-comic-widget/main/assets/audio/Boing.m4a"
     }
   ]
 };
