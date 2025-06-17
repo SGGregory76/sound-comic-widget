2ab21035fe1c0bed6cd6a0e0e8a87a712b13bdb6
SGGregory76 <sggregory76@gmail.com>
2025-06-16 20:25:48 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 window.CONFIG = {
   scenes: [
     {
-      image: "assets/images/chicken.png",
-      sound: "assets/audio/squawk.mp3"
+      image: "assets/images/2a82ade3-569d-4a55-af3a-7e61d8306add.png",
+      sound: "assets/audio/arnold-schwarzenegger---i-want-to-ask-you-a-bunch-of-questions-made-with-Voicemod.mp3"
     },
     {
-      image: "assets/images/pizza.png",
-      sound: "assets/audio/yum.mp3"
+      image: "assets/images/ecbbc098-2535-48f1-acfa-ec26fad2c7d2.png",
+      sound: "assets/audio/epic-logo-6906.mp3"
     },
     {
-      image: "assets/images/burrito.png",
-      sound: "assets/audio/boom.mp3"
+      image: "assets/images/36563c55-2a48-43db-8851-4289a46e3763.png",
+      sound: "assets/audio/arnold-schwarzenegger---i-want-to-ask-you-a-bunch-of-questions-made-with-Voicemod.mp3"
     }
   ]
 };
